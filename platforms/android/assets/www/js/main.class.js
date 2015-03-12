b72ac19c69532fb3388ac14f8811a576fb3c703b
Sander Bergman <proactivewebsites@gmail.com>
2015-03-12 16:36:02 +0100

    .

---
+++ platforms/android/assets/www/js/main.class.js
@@ -0,0 +1,15 @@
+
+var MAIN = Class.create();
+MAIN.prototype = 
+{
+	initialize: function() 
+	{
+		
+	},
+	load: function()
+	{
+		navigator.notification.alert("Prototyped message box");
+	}
+};
+
+
--- platforms/windows/www/js/index.js
+++ platforms/windows/www/js/index.js
@@ -1,4 +1,4 @@
-
+﻿
 var app = {
     // Application Constructor
     initialize: function() 
